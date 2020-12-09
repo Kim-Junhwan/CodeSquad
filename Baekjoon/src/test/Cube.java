bb3b250a7f7ecfb4cd81c7813ee38e7b485953f2
Kim Junhwan <tazanboy1@naver.com>
2020-12-10 00:57:05 +0900

    2번문제 전역변수 요구사항 적용.
    threecube를 전역변수로 사용하지 않고 class를 따로 생성하고 객체로 사용. 입력받은 문자열중 " ' "가 들어간 문자열을 아직 인식 못함, 전 커밋에 까먹고 안씀.

--- Baekjoon/src/test/Cube.java
+++ Baekjoon/src/test/Cube.java
@@ -5,78 +5,76 @@ import java.util.Deque;
 import java.util.Scanner;
 
 public class Cube {
-	String[][] threecube= {
-			{"R","R","W"},
-			{"G","C","W"},
-			{"G","B","B"}
-	};
+	CubeObject obj=new CubeObject();
+	
 	 Deque<String> dq;
 	Scanner scanner=new Scanner(System.in);
 	String[] list;
 	String input;
 	
 	public Cube() {
-		print();
+		String[][] threecube=obj.getThreecube();
+		print(threecube);
 		while(true) {
 			System.out.print("CUBE >");
 			list=scanner.next().split("");
-			move();
+			move(threecube);
 		}
 	}
 	
-	public void move() {
+	public void move(String[][] threecube) {
 		for(int i=0;i<list.length;i++) {
 			dq=new ArrayDeque<String>();
 			if(list[i].equals("U")) {
-				dq=inputdeque("width",0,0);
+				dq=inputdeque(threecube,"width",0,0);
 				dq=directL(dq);
-				inputcube(dq,"width",0,0);
+				inputcube(threecube,dq,"width",0,0);
 			}
 			else if(list[i].equals("U\'")) {
-				dq=inputdeque("width",0,0);
+				dq=inputdeque(threecube,"width",0,0);
 				dq=directR(dq);
-				inputcube(dq,"width",0,0);
+				inputcube(threecube,dq,"width",0,0);
 			}
 			else if(list[i].equals("R")) {
-				dq=inputdeque("high",0,2);
+				dq=inputdeque(threecube,"high",0,2);
 				dq=directL(dq);
-				inputcube(dq,"high",0,2);
+				inputcube(threecube,dq,"high",0,2);
 			}
 			else if(list[i].equals("R\'")) {
-				dq=inputdeque("high",0,2);
+				dq=inputdeque(threecube,"high",0,2);
 				dq=directR(dq);
-				inputcube(dq,"high",0,2);
+				inputcube(threecube,dq,"high",0,2);
 			}
 			else if(list[i].equals("L")) {
-				dq=inputdeque("high",0,0);
+				dq=inputdeque(threecube,"high",0,0);
 				dq=directR(dq);
-				inputcube(dq,"high",0,0);
+				inputcube(threecube,dq,"high",0,0);
 			}
 			else if(list[i].equals("L'")) {
-				dq=inputdeque("high",0,0);
+				dq=inputdeque(threecube,"high",0,0);
 				dq=directL(dq);
-				inputcube(dq,"high",0,0);
+				inputcube(threecube,dq,"high",0,0);
 			}
 			else if(list[i].equals("B")) {
-				dq=inputdeque("width",2,0);
+				dq=inputdeque(threecube,"width",2,0);
 				dq=directR(dq);
-				inputcube(dq,"width",2,0);
+				inputcube(threecube,dq,"width",2,0);
 			}
 			else if(list[i].equals("B'")) {
-				dq=inputdeque("width",2,0);
+				dq=inputdeque(threecube,"width",2,0);
 				dq=directL(dq);
-				inputcube(dq,"width",2,0);
+				inputcube(threecube,dq,"width",2,0);
 			}
 			else if(list[i].equals("Q")) {
 				System.out.println("Bye~");
 				System.exit(0);
 			}
 			System.out.println(list[i]);
-			print();
+			print(threecube);
 		}
 	}
 	
-	Deque directR(Deque dq){
+	Deque directR(Deque dq){	//오른쪽으로 문자열 이동
 		for(int i=0;i<1;i++) {
 			String word=(String)dq.pollLast();
 			dq.addFirst(word);
@@ -84,7 +82,7 @@ public class Cube {
 		return dq;
 	}
 	
-	Deque directL(Deque dq){
+	Deque directL(Deque dq){	//왼쪽으로 문자열 이동
 		for(int i=0;i<1;i++) {
 			String word=(String)dq.pollFirst();
 			dq.addLast(word);
@@ -92,7 +90,7 @@ public class Cube {
 		return dq;
 	}
 	
-	public Deque inputdeque(String cross, int row,int column) {
+	public Deque inputdeque(String[][] threecube,String cross, int row,int column) { //deque에 cube내용 넣기
 		if(cross.equals("high")) {
 			for(int i=0;i<3;i++) {
 				dq.add(threecube[i][column]);
@@ -106,22 +104,22 @@ public class Cube {
 		return dq;
 	}
 	
-	public void inputcube(Deque dq,String cross,int row,int column) {
+	public void inputcube(String[][] threecube,Deque dq,String cross,int row,int column) {	//deque에 의해 이동된 문자열 threecube에 넣기
 		if(cross.equals("high")) {
-			for(int i=0;i<3;i++) {
+			for(int i=0;i<threecube.length;i++) {
 				threecube[i][column]=(String) dq.pollFirst();
 			}
 		}
 		else if(cross.equals("width")) {
-			for(int i=0;i<3;i++) {
+			for(int i=0;i<threecube.length;i++) {
 				threecube[row][i]=(String) dq.pollFirst();
 			}
 		}
 	}
 	
-	public void print() {
-		for (int i = 0; i < threecube.length; i++) {
-            for (int j = 0; j < threecube.length; j++) {
+	public void print(String[][] threecube) {
+		for (int i=0;i <threecube.length;i++) {
+            for (int j=0;j<threecube.length;j++) {
                 System.out.print(threecube[i][j]);
             }
             System.out.println();
---
+++ Baekjoon/src/test/CubeObject.java
@@ -0,0 +1,18 @@
+package test;
+
+public class CubeObject {
+	String[][] threecube= {
+			{"R","R","W"},
+			{"G","C","W"},
+			{"G","B","B"}
+	};
+
+	public String[][] getThreecube() {
+		return threecube;
+	}
+
+	public void setThreecube(String[][] threecube) {
+		this.threecube = threecube;
+	}
+	
+}
